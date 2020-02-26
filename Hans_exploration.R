c3e2086e7f04de1466295e40e20437d906286066
Dai Shizuka <daishizuka@gmail.com>
2020-02-26 13:11:00 -0600

    code update

--- Hans_exploration.R
+++ Hans_exploration.R
@@ -7,17 +7,18 @@ load("all_visits.dat")
 head(all_visits)
 
 all_visits$Date
-wbnu=all_visits %>% filter(Species=="WBNU") %>% arrange(RFID, Logger) 
 
-wbnu.visits.daily=wbnu%>% group_by(RFID, Date) %>% count()
+usedat=all_visits %>% filter(Species=="WBNU"|Species=="DOWO") %>% arrange(RFID, Logger) 
 
-##wbnu.visits.daily is the total number of visits to any feeder for a given bird on a given day
+visits.daily=usedat%>% group_by(RFID, Date) %>% count()
+
+##visits.daily is the total number of visits to any feeder for a given bird on a given day
 
 
-wbnu.visits.daily.feeders=wbnu%>% group_by(RFID, Date) %>% select(RFID, Date, Logger) %>% summarise(n_distinct(Logger))
+visits.daily.feeders=usedat%>% group_by(RFID, Date) %>% select(RFID, Date, Logger) %>% summarise(n_distinct(Logger))
 ##wbnu.visits.daily.feeders is the number of distinct feeders used by each bird on a given day
 
-logger_array=wbnu%>% group_by(RFID, Date) %>% select(Logger,  Date,RFID) %>% table()
+logger_array=usedat%>% group_by(RFID, Date) %>% select(Logger,  Date,RFID) %>% table()
 
 apply(logger_array, 3, function(x) {
   if (colSums(x)==0) p=NA else p=x/colSums(x)
